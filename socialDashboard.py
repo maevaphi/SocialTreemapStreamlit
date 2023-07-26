0ec83977f83c4b3118cd06f6ff344d979fb0f429
Maeva <72557919+maevaphi@users.noreply.github.com>
2023-07-26 17:04:42 +0200

    Update socialDashboard.py

--- socialDashboard.py
+++ socialDashboard.py
@@ -15,10 +15,10 @@ from PIL import Image
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 
-#st.title("Social impacts of mining raw materials for battery active materials")
+st.title("Social impacts of mining raw materials for battery active materials")
 
-img = Image.open("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/LCMtitle.jpg")
-st.image(img)
+#img = Image.open("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/LCMtitle.jpg")
+#st.image(img)
 
 'Maeva LAVIGNE PHILIPPOT, Joeri VAN MIERLO, Maarten MESSAGIE'
 'VUB, Belgium'
@@ -33,7 +33,7 @@ option = st.selectbox(
     'Which cathode active material do you want to visualize?',
      df['first column'])
 
-# 'You selected: ', option
+'You selected: ', option
 
 NMC622 = pd.read_csv("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/NMC622Treemap.csv")
 NMC811 = pd.read_csv("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/NMC811Treemap.csv")
@@ -56,9 +56,9 @@ LNMO = pd.read_csv("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brusse
 
 st.divider()
 
-logo = Image.open("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/AM4BAT - Brand PNG.png")
-st.image(logo, width=200)
+#logo = Image.open("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/AM4BAT - Brand PNG.png")
+#st.image(logo, width=200)
 
 'This project has received funding from the European Union Horizon 2020 research and innovation programme under grant agreement No 101069756.'
-flag = Image.open("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/EU.jpg")
-st.image(flag, width=100)
+#flag = Image.open("C:/Users/mphilip1_admin/OneDrive - Vrije Universiteit Brussel/thèse/python/EU.jpg")
+#st.image(flag, width=100)
