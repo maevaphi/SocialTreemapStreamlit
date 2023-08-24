2d43d16828ee9e3798fa504957eb2950f3db9b45
Maeva <72557919+maevaphi@users.noreply.github.com>
2023-08-24 14:34:33 +0200

    Update socialDashboard.py

--- socialDashboard.py
+++ socialDashboard.py
@@ -108,9 +108,9 @@ if option == "all":
 'The indicators in PSILCA database are affected 6 level of risks from no risk (blue) to very high risk (red).'
 
 with st.expander("See explanation"):
-    st.write(The size of the areas indicates the material amount and the country s market share.)
-    st.write(Each stakeholder is allocated to the same area, even though the number of indicators is different.)
-    st.write(The indicators in PSILCA database are affected 6 level of risks from no risk (blue) to very high risk (red).)
+    st.write("The size of the areas indicates the material amount and the country s market share.")
+    st.write("Each stakeholder is allocated to the same area, even though the number of indicators is different.")
+    st.write("The indicators in PSILCA database are affected 6 level of risks from no risk (blue) to very high risk (red).")
 
 'maeva.philippot@vub.be'
 
