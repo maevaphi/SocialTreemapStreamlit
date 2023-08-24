afb7e17b38c5724a4d0e12ddec715e0dcd50c5a2
Maeva <72557919+maevaphi@users.noreply.github.com>
2023-08-24 16:34:12 +0200

    Update socialDashboard.py

--- socialDashboard.py
+++ socialDashboard.py
@@ -19,8 +19,8 @@ st.image(img)
 
 
 df = pd.DataFrame({
-    'first column': [ ,'NMC622 (LiNi0.8Mn0.1Co0.1O2)', 'NMC811 (LiNi0.8Mn0.1Co0.1O2)', 'LNMO (Li2NiMn3O8)', 'all'],
-    'second column': [1, 2, 3, 4]
+    'first column': [,'NMC622 (LiNi0.8Mn0.1Co0.1O2)', 'NMC811 (LiNi0.8Mn0.1Co0.1O2)', 'LNMO (Li2NiMn3O8)', 'all'],
+    'second column': [0,1, 2, 3, 4]
     })
 
 option = st.selectbox(
